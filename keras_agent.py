fa45dd2684303e09d55efb81e3067185d861b537
Harsh Agrawal <hrsh.agrwl1@gmail.com>
2019-04-24 19:49:53 -0400

    Changed implementation to tensorflow slim

--- keras_agent.py
+++ keras_agent.py
@@ -1,24 +1,15 @@
+import sys
 import numpy as np
 import itertools as it
 import random
 from skimage import color, transform
 from collections import namedtuple, deque
 
-import tensorflow.contrib.slim as slim
 import tensorflow as tf
-
-import sys
+import tensorflow.contrib.slim as slim
 
 # from model import DQN
 from experience_history import History
-
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation
-from keras.layers import BatchNormalization
-from keras.layers import Conv2D, MaxPooling2D
-from keras.layers import Flatten
-from keras.layers import Dropout
-from keras.optimizers import Adam
 
 class DQNAgent:
   def __init__(self, env):
@@ -37,12 +28,14 @@ class DQNAgent:
     self.epsilon_decay_steps = int(1e5)
     self.learning_rate = 4e-4
     self.tau = 1
+    self.regularization = 1e-6
+    self.optimizer_params = dict(learning_rate=0.0004, epsilon=1e-7)
 
     # Flags
     self.network_update_frequency = int(1e3)
     self.train_freq = 4
     self.frame_skip = 3
-    self.min_experience_size = 64
+    self.min_experience_size = int(1e4)
     
     # Enviroment
     self.render = True 
@@ -52,10 +45,6 @@ class DQNAgent:
     acceleration = [1, 0]
     brake = [0.2, 0]
     all_actions = np.array([action for action in it.product(left_right, acceleration, brake)])
-
-    # defined_actions = [[-1, 0, 0], [0, 0, 0], [1, 0, 0], [0, 1, 0], [0, 0, 0.3]]
-    # all_actions = np.array(defined_actions)
-
     self.action_map = all_actions
     self.num_actions = len(self.action_map)
     gas_actions = np.array([a[1] == 1 and a[2] == 0 for a in self.action_map])
@@ -64,23 +53,10 @@ class DQNAgent:
     self.action_weights /= np.sum(self.action_weights)
     print('Action Map -> ', self.action_map)
     
-    # Model (Neural Network)
-    # self.training_model = DQN(self.num_actions)
-    # self.target_model = DQN(self.num_actions)
+    # Build the model
+    self.build_model()
+    self.session = None
 
-    self.model = self._get_model()
-    self.target_model = self._get_model()
-
-    # # Load models to GPU
-    # if torch.cuda.is_available():
-    #   self.training_model.cuda()
-    #   self.target_model.cuda()
-    
-    # self.optimizer = optim.Adam(self.training_model.parameters(), lr = self.learning_rate)
-
-    print('---------- Model ---------')
-    print(self.model.summary())
-    
     # Negative Reward
     # To check if we want to end the episode earlier
     self.neg_reward_counter = 0
@@ -91,21 +67,68 @@ class DQNAgent:
     self.memory = History(self.num_frame_stack, self.experience_capacity)
     self.network_chosen_action = 0
   
-  def _get_model(self):
-    model = Sequential()
-    model.add(Conv2D(filters = 8, kernel_size = 3, activation='relu', 
-      input_shape=(self.num_frame_stack,) + self.image_size, data_format='channels_first'))
-    model.add(BatchNormalization())
-    model.add(MaxPooling2D(pool_size = (2, 2), dim_ordering="th"))
-    model.add(Conv2D(filters = 16, kernel_size = 3, activation='relu'))
-    model.add(BatchNormalization())
-    model.add(MaxPooling2D(pool_size = (2, 2), dim_ordering="th"))
-    model.add(Flatten())
-    # model.add(Dropout(0.2))
-    model.add(Dense(256, activation='relu'))
-    model.add(Dense(self.num_actions))
-    model.compile(optimizer=Adam(lr=self.learning_rate), loss = 'mse')
-    return model
+
+  def build_model(self):
+    input_dim_with_batch = (self.batch_size, self.num_frame_stack) + self.image_size
+    input_dim_general = (None, self.num_frame_stack) + self.image_size
+
+    self.input_prev_state = tf.placeholder(tf.float32, input_dim_general, "prev_state")
+    self.input_next_state = tf.placeholder(tf.float32, input_dim_with_batch, "next_state")
+    self.input_reward = tf.placeholder(tf.float32, self.batch_size, "reward")
+    self.input_actions = tf.placeholder(tf.int32, self.batch_size, "actions")
+    self.input_done_mask = tf.placeholder(tf.int32, self.batch_size, "done_mask")
+
+    # These are the state action values for all states
+    # The target Q-values come from the fixed network
+    with tf.variable_scope("fixed"):
+      qsa_targets = self.create_network(self.input_next_state, trainable=False)
+
+    with tf.variable_scope("train"):
+      qsa_estimates = self.create_network(self.input_prev_state, trainable=True)
+
+    self.best_action = tf.argmax(qsa_estimates, axis=1)
+
+    not_done = tf.cast(tf.logical_not(tf.cast(self.input_done_mask, "bool")), "float32")
+    q_target = tf.reduce_max(qsa_targets, -1) * self.gamma * not_done + self.input_reward
+    # select the chosen action from each row
+    # in numpy this is qsa_estimates[range(batchsize), self.input_actions]
+    action_slice = tf.stack([tf.range(0, self.batch_size), self.input_actions], axis=1)
+    q_estimates_for_input_action = tf.gather_nd(qsa_estimates, action_slice)
+
+    training_loss = tf.nn.l2_loss(q_target - q_estimates_for_input_action) / self.batch_size
+
+    optimizer = tf.train.AdamOptimizer(**(self.optimizer_params))
+
+    reg_loss = tf.add_n(tf.losses.get_regularization_losses())
+    self.train_op = optimizer.minimize(reg_loss + training_loss)
+
+    train_params = self.get_variables("train")
+    fixed_params = self.get_variables("fixed")
+
+    self.copy_network_ops = [tf.assign(fixed_v, train_v) for train_v, fixed_v in zip(train_params, fixed_params)]
+
+  def get_variables(self, scope):
+    vars = [t for t in tf.global_variables()
+        if "%s/" % scope in t.name and "Adam" not in t.name]
+    return sorted(vars, key=lambda v: v.name)
+
+  def create_network(self, input, trainable):
+    if trainable:
+      wr = slim.l2_regularizer(self.regularization)
+    else:
+      wr = None
+
+    # the input is stack of black and white frames.
+    # put the stack in the place of channel (last in tf)
+    input_t = tf.transpose(input, [0, 2, 3, 1])
+    net = slim.conv2d(input_t, 8, (7, 7), data_format="NHWC", activation_fn=tf.nn.relu, stride=3, weights_regularizer=wr, trainable=trainable)
+    net = slim.max_pool2d(net, 2, 2)
+    net = slim.conv2d(net, 16, (3, 3), data_format="NHWC", activation_fn=tf.nn.relu, weights_regularizer=wr, trainable=trainable)
+    net = slim.max_pool2d(net, 2, 2)
+    net = slim.flatten(net)
+    net = slim.fully_connected(net, 256, activation_fn=tf.nn.relu, weights_regularizer=wr, trainable=trainable)
+    q_state_action_values = slim.fully_connected(net, self.num_actions, activation_fn=None, weights_regularizer=wr, trainable=trainable)
+    return q_state_action_values
 
   def step(self, state, action, reward, done):
     # Save experience in replay memory
@@ -118,64 +141,26 @@ class DQNAgent:
         experiences = self.memory.sample(self.batch_size)
         self.learn(experiences)
 
-  def get_action(self):
-    # Epsilon-greedy action selection
-
-    if random.random() > self.get_epsilon():
-      # self.network_chosen_action += 1
-      # # Add the batch dimension before creating a tensor
-      state = self.memory.current_state()[np.newaxis, ...]
-      # state = torch.from_numpy(state).float().to(device)
-      # self.training_model.eval()
-      # with torch.no_grad():
-      #   action_values = self.training_model(state)
-        
-      # self.training_model.train()
-      # action = np.argmax(action_values.cpu().data.numpy())
-      act_values = self.model.predict(state)
-      action = np.argmax(act_values[0])
-      self.network_chosen_action += 1
-      return action
-      # return action
-    else:
-      return self.get_random_action()
-
-  def get_action_for_state(self, state):
-    act_values = self.model.predict(state[np.newaxis, ...])
-    action = np.argmax(act_values[0])
-    return action
-
   def learn(self, experiences):
     states, actions, rewards, next_states, dones = experiences
-    Q_target_next = np.amax(self.target_model.predict(next_states), axis = 1)
-    Q_target = rewards + self.gamma * Q_target_next * (1 - dones)
-    Q_expected = self.model.predict(states)
-    for i, a_idx in enumerate(actions):
-      Q_expected[i, a_idx] = Q_target[i]
-    self.model.fit(states, Q_expected, epochs=1, verbose=0)
+    fd = {
+        self.input_reward: rewards,
+        self.input_prev_state: states,
+        self.input_next_state: next_states,
+        self.input_actions: actions,
+        self.input_done_mask: dones
+    }
+    self.session.run([self.train_op], fd)
 
-    # if not done:
-    #   # predict the future discounted reward
-    # target = reward + self.gamma * np.amax(self.model.predict(next_state)[0])
-    #   # make the agent to approximately map
-    #   # the current state to future discounted reward
-    #   # We'll call that target_f
-    #   target_f = self.model.predict(state)
-    #   target_f[0][action] = target
-    #   # Train the Neural Net with the state and target_f
-    
-
-    # ------------------- update target network ------------------- #
-    if self.global_counter % self.network_update_frequency == 0:
-      self.soft_update()
-                        
-
-  def soft_update(self):
-    weights = self.model.get_weights()
-    target_weights = self.target_model.get_weights()
-    for i in range(len(target_weights)):
-      target_weights[i] = weights[i] * self.tau + target_weights[i] * (1 - self.tau)
-    self.target_model.set_weights(target_weights)
+  def get_action(self):
+    # Epsilon-greedy action selection
+    if np.random.rand() > self.get_epsilon():
+      self.network_chosen_action += 1
+      action_idx = self.session.run(self.best_action,
+          { self.input_prev_state: self.memory.current_state()[np.newaxis, ...] })[0]
+      return action_idx
+    else:
+      return np.random.choice(self.num_actions, p=self.action_weights)            
           
   def get_epsilon(self):
     if self.global_counter >= self.epsilon_decay_steps:
@@ -189,8 +174,6 @@ class DQNAgent:
     total_reward = 0
     frames_in_episode = 0
 
-    # self.soft_update(self.training_model, self.target_model) 
-    
     state = self.env.reset()
     state = self.process_image(state)
     self.memory.start_new_episode(state)
@@ -198,6 +181,7 @@ class DQNAgent:
     while True:
       self.global_counter += 1
       frames_in_episode += 1
+
       action_idx = self.get_action()
       action = self.action_map[action_idx]
       reward = 0
@@ -225,24 +209,18 @@ class DQNAgent:
       state = next_state
       total_reward += reward
 
+      if self.global_counter % self.network_update_frequency:
+        self.session.run(self.copy_network_ops)
+
       if done:
         break
-  
+      
     return total_reward, frames_in_episode
     
-  
   # Convert RGB Image to grayscale and channel dimension
   def process_image(self, img):
-    i = 2 * color.rgb2gray(img) - 1
-    # return i[np.newaxis, ...]
-    return i
-    # i = np.swapaxes(img, 0, 2)
-    # return (i - 128) / 128.0
+    return 2 * color.rgb2gray(img) - 1
 
-  # Returns a random action.
-  def get_random_action(self):
-    return np.random.choice(self.num_actions, p=self.action_weights)
-  
   def check_early_stop(self, reward, total_reward):
     if reward < 0:
       self.neg_reward_counter += 1
